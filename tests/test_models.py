c872cbda716e48c9014142edf0ee036e67a4d677
Zakir Abdullayev <zakirabdullayev8@gmail.com>
2024-08-17 11:44:17 +0400

    test: operand factory

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,15 @@
 import pytest
 
-from src.pytemplate.domain.models import Operands
+from src.pytemplate.domain.models import Operands, operands_factory
 
 
 def test_model_created():
     operands = Operands(first_operand=1, second_operand=2)
     assert operands.first_operand == 1
     assert operands.second_operand == 2
+
+
+def test_factory_created():
+    operands = operands_factory(1, 2)
+    assert isinstance(operands, Operands)
+    assert type(operands) == Operands
