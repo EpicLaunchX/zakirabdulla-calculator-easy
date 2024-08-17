351ddb7589c078dbe78f3e5d1e75b52bb7b78764
Zakir Abdullayev <zakirabdullayev8@gmail.com>
2024-08-17 12:08:10 +0400

    fix: using factory

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -5,24 +5,24 @@ from src.pytemplate.service.calculator import Calculator
 
 
 def test_calculator_add():
-    result = Calculator().add(Operands(1, 2))
+    result = Calculator().add(operands_factory(1, 2))
     assert result == 3
     assert type(result) == int
 
 
 def test_calculator_subtract():
-    result = Calculator().subtract(Operands(2, 1))
+    result = Calculator().subtract(operands_factory(2, 1))
     assert result == 1
     assert type(result) == int
 
 
 def test_calculator_multiply():
-    result = Calculator().multiply(Operands(2, 3))
+    result = Calculator().multiply(operands_factory(2, 3))
     assert result == 6
     assert type(result) == int
 
 
 def test_calculator_divide():
-    result = Calculator().divide(Operands(6, 3))
+    result = Calculator().divide(operands_factory(6, 3))
     assert result == 2
     assert type(result) == int
