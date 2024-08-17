5212017b2ce56e1aa15d00fd12ebcb327e692b80
Zakir Abdullayev <zakirabdullayev8@gmail.com>
2024-08-17 12:42:52 +0400

    fix:fixing patch

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,31 +5,31 @@ import pytest
 from src.pytemplate.entrypoints.cli.main import main
 
 
-@mock.patch("builtins.input", return_value="1,2,add")
-def test_main_add(mock_input):
-    result = main()
-    assert result == 3
+def test_main_add():
+    with mock.patch("builtins.input", return_value="1,2,add") as mock_result:
+        result = main()
+        assert result == 3
 
 
-@mock.patch("builtins.input", return_value="2,1,subtract")
-def test_main_subtract(mock_input):
-    result = main()
-    assert result == 1
+def test_main_subtract():
+    with mock.patch("builtins.input", return_value="2,1,subtract"):
+        result = main()
+        assert result == 1
 
 
-@mock.patch("builtins.input", return_value="2,3,multiply")
-def test_main_multiply(mock_input):
-    result = main()
-    assert result == 6
+def test_main_multiply():
+    with mock.patch("builtins.input", return_value="2,3,multiply"):
+        result = main()
+        assert result == 6
 
 
-@mock.patch("builtins.input", return_value="6,3,divide")
-def test_main_divide(mock_input):
-    result = main()
-    assert result == 2
+def test_main_divide():
+    with mock.patch("builtins.input", return_value="6,3,divide") as mock_result:
+        result = main()
+        assert result == 2
 
 
-@mock.patch("builtins.input", return_value="6,3,invalid")
-def test_main_invalid(mock_input):
-    with pytest.raises(ValueError):
-        main()
+def test_main_invalid():
+    with mock.patch("builtins.input", return_value="6,3,invalid") as mock_result:
+        with pytest.raises(ValueError):
+            main()
